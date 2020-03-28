cb106257489ca61968491a5e9ef8e74212ac4316
Andy Smith <andysmith836@gmail.com>
2020-03-28 22:48:12 +0000

    Extract some functions

--- src/main/kotlin/com/andy/RomanNumerals.kt
+++ src/main/kotlin/com/andy/RomanNumerals.kt
@@ -9,61 +9,77 @@ class RomanNumerals {
 
         // Units of 1
         if (i / 1 < 10) {
-            if (i == 5 - 1) {
-                return "IV"
-            }
-
-            if (i < 5) {
-                return "I".repeat(i)
-            }
-
-            if (i > 5 && i < 10 - 1) {
-                return "V" + "I".repeat(i - 5)
-            }
-
-            return "IX"
+            return convertUnitsOfOne(i)
         }
 
         // Units of 10
         if (i / 10 < 10) {
-            if (i == 50 - 10) {
-                return "XL"
-            }
-
-            if (i < 50) {
-                return "X".repeat(i / 10)
-            }
-
-            if (i > 50 && i < 100 - 10) {
-                return "L" + "X".repeat(i / 10 - 50 / 10)
-            }
-
-            return "XC"
+            return convertUnitsOfTen(i)
         }
 
         // units of 100
         if (i / 100 < 10) {
-            if (i == 500 - 100) {
-                return "CD"
-            }
-
-            if (i < 500) {
-                return "C".repeat(i / 100)
-            }
-
-            if (i > 500 && i < 1000 - 100) {
-                return "D" + "C".repeat(i / 100 - 500 / 100)
-            }
-
-            return "CM"
+            return convertUnitsOf100(i)
         }
 
         // units of 1000
         if (i / 1000 < 10) {
-            return "M".repeat(i / 1000)
+            return convertUnitsOf1000(i)
         }
 
         return ""
+    }
+
+    private fun convertUnitsOf1000(i: Int): String {
+        return "M".repeat(i / 1000)
+    }
+
+    private fun convertUnitsOf100(i: Int): String {
+        if (i == 500 - 100) {
+            return "CD"
+        }
+
+        if (i < 500) {
+            return "C".repeat(i / 100)
+        }
+
+        if (i > 500 && i < 1000 - 100) {
+            return "D" + "C".repeat(i / 100 - 500 / 100)
+        }
+
+        return "CM"
+    }
+
+    private fun convertUnitsOfTen(i: Int): String {
+        if (i == 50 - 10) {
+            return "XL"
+        }
+
+        if (i < 50) {
+            return "X".repeat(i / 10)
+        }
+
+        if (i > 50 && i < 100 - 10) {
+            return "L" + "X".repeat(i / 10 - 50 / 10)
+        }
+
+        return "XC"
+    }
+
+    private fun convertUnitsOfOne(i: Int): String {
+        if (i == 5 - 1) {
+            return "IV"
+        }
+
+        if (i < 5) {
+            return "I".repeat(i)
+        }
+
+        if (i > 5 && i < 10 - 1) {
+            return "V" + "I".repeat(i - 5)
+        }
+
+        return "IX"
     }
 
     private fun convertBaseSymbols(i: Int): String? {
