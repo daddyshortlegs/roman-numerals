f6419d194fd427f816ce58c9cad9fae6730dd925
Andy Smith <andysmith836@gmail.com>
2020-03-28 22:55:36 +0000

    Pull out some more variables

--- src/main/kotlin/com/andy/RomanNumerals.kt
+++ src/main/kotlin/com/andy/RomanNumerals.kt
@@ -32,16 +32,17 @@ class RomanNumerals {
 
     private fun convertUnitsOf1(i: Int): String {
         val unitsOf1 = 1
-        if (i == 5 - unitsOf1) {
+        val by5 = 5 * unitsOf1
+        if (i == by5 - unitsOf1) {
             return "IV"
         }
 
-        if (i < 5) {
+        if (i < by5) {
             return "I".repeat(i / unitsOf1)
         }
 
-        if (i > 5 && i < 10 - unitsOf1) {
-            return "V" + "I".repeat(i / unitsOf1 - 5 / unitsOf1)
+        if (i > by5 && i < 10 - unitsOf1) {
+            return "V" + "I".repeat(i / unitsOf1 - by5 / unitsOf1)
         }
 
         return "IX"
@@ -49,16 +50,17 @@ class RomanNumerals {
 
     private fun convertUnitsOf10(i: Int): String {
         val unitsOf10 = 10
-        if (i == 50 - unitsOf10) {
+        val by50 = 5 * unitsOf10
+        if (i == by50 - unitsOf10) {
             return "XL"
         }
 
-        if (i < 50) {
+        if (i < by50) {
             return "X".repeat(i / unitsOf10)
         }
 
-        if (i > 50 && i < 100 - unitsOf10) {
-            return "L" + "X".repeat(i / unitsOf10 - 50 / unitsOf10)
+        if (i > by50 && i < 100 - unitsOf10) {
+            return "L" + "X".repeat(i / unitsOf10 - by50 / unitsOf10)
         }
 
         return "XC"
@@ -66,16 +68,17 @@ class RomanNumerals {
 
     private fun convertUnitsOf100(i: Int): String {
         val unitsOf100 = 100
-        if (i == 500 - unitsOf100) {
+        val by500 = 5 * unitsOf100
+        if (i == by500 - unitsOf100) {
             return "CD"
         }
 
-        if (i < 500) {
+        if (i < by500) {
             return "C".repeat(i / unitsOf100)
         }
 
-        if (i > 500 && i < 1000 - unitsOf100) {
-            return "D" + "C".repeat(i / unitsOf100 - 500 / unitsOf100)
+        if (i > by500 && i < 1000 - unitsOf100) {
+            return "D" + "C".repeat(i / unitsOf100 - by500 / unitsOf100)
         }
 
         return "CM"
