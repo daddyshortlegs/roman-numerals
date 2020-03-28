cc09ab860c8e56032cb09cf4d9bbbd965db57405
Andy Smith <andysmith836@gmail.com>
2020-03-28 22:53:13 +0000

    Refactor some common constants

--- src/main/kotlin/com/andy/RomanNumerals.kt
+++ src/main/kotlin/com/andy/RomanNumerals.kt
@@ -31,55 +31,59 @@ class RomanNumerals {
     }
 
     private fun convertUnitsOf1(i: Int): String {
-        if (i == 5 - 1) {
+        val unitsOf1 = 1
+        if (i == 5 - unitsOf1) {
             return "IV"
         }
 
         if (i < 5) {
-            return "I".repeat(i)
+            return "I".repeat(i / unitsOf1)
         }
 
-        if (i > 5 && i < 10 - 1) {
-            return "V" + "I".repeat(i - 5)
+        if (i > 5 && i < 10 - unitsOf1) {
+            return "V" + "I".repeat(i / unitsOf1 - 5 / unitsOf1)
         }
 
         return "IX"
     }
 
     private fun convertUnitsOf10(i: Int): String {
-        if (i == 50 - 10) {
+        val unitsOf10 = 10
+        if (i == 50 - unitsOf10) {
             return "XL"
         }
 
         if (i < 50) {
-            return "X".repeat(i / 10)
+            return "X".repeat(i / unitsOf10)
         }
 
-        if (i > 50 && i < 100 - 10) {
-            return "L" + "X".repeat(i / 10 - 50 / 10)
+        if (i > 50 && i < 100 - unitsOf10) {
+            return "L" + "X".repeat(i / unitsOf10 - 50 / unitsOf10)
         }
 
         return "XC"
     }
 
     private fun convertUnitsOf100(i: Int): String {
-        if (i == 500 - 100) {
+        val unitsOf100 = 100
+        if (i == 500 - unitsOf100) {
             return "CD"
         }
 
         if (i < 500) {
-            return "C".repeat(i / 100)
+            return "C".repeat(i / unitsOf100)
         }
 
-        if (i > 500 && i < 1000 - 100) {
-            return "D" + "C".repeat(i / 100 - 500 / 100)
+        if (i > 500 && i < 1000 - unitsOf100) {
+            return "D" + "C".repeat(i / unitsOf100 - 500 / unitsOf100)
         }
 
         return "CM"
     }
 
     private fun convertUnitsOf1000(i: Int): String {
-        return "M".repeat(i / 1000)
+        val unitsOf1000 = 1000
+        return "M".repeat(i / unitsOf1000)
     }
 
     private fun convertBaseSymbols(i: Int): String? {
