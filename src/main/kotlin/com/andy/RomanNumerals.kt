10814f9238982fefc7ee75de9e6571418e8198fb
Andy Smith <andysmith836@gmail.com>
2020-03-28 21:20:44 +0000

    Added all base symbols

--- src/main/kotlin/com/andy/RomanNumerals.kt
+++ src/main/kotlin/com/andy/RomanNumerals.kt
@@ -4,6 +4,16 @@ class RomanNumerals {
     fun convert(i: Int): String {
         if (i == 5) {
             return "V"
+        } else if (i == 10) {
+            return "X"
+        } else if (i == 50) {
+            return "L"
+        } else if (i == 100) {
+            return "C"
+        } else if (i == 500) {
+            return "D"
+        } else if (i == 1000) {
+            return "M"
         }
         return "I"
     }
--- src/test/kotlin/com/andy/RomanNumeralsTest.kt
+++ src/test/kotlin/com/andy/RomanNumeralsTest.kt
@@ -10,5 +10,10 @@ class RomanNumeralsTest {
     fun `should convert 1`() {
         assertEquals("I", romanNumerals.convert(1))
         assertEquals("V", romanNumerals.convert(5))
+        assertEquals("X", romanNumerals.convert(10))
+        assertEquals("L", romanNumerals.convert(50))
+        assertEquals("C", romanNumerals.convert(100))
+        assertEquals("D", romanNumerals.convert(500))
+        assertEquals("M", romanNumerals.convert(1000))
     }
 }
