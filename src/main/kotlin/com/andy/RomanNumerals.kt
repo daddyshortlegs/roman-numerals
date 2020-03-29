a08a170000c353f8a2003649f8b41daf11c9fc29
Andy Smith <andysmith836@gmail.com>
2020-03-29 22:37:59 +0100

    Simplify slightly

--- src/main/kotlin/com/andy/RomanNumerals.kt
+++ src/main/kotlin/com/andy/RomanNumerals.kt
@@ -4,13 +4,12 @@ class RomanNumerals {
     fun convert(value: Int): String {
         var i = value
         var romanNumber = ""
-        var divisor = 1000
-        while (divisor > 0) {
+
+        for (divisor in listOf(1000, 100, 10, 1)) {
             if (i / divisor in 1..9) {
                 romanNumber += convertUnitsOf(divisor, i)
                 i -= (i / divisor) * divisor
             }
-            divisor /= 10
         }
 
         return romanNumber
@@ -18,32 +17,36 @@ class RomanNumerals {
 
     private fun convertUnitsOf(units: Int, i: Int): String {
         val midPoint = 5 * units
-        val romanNumeralForUnits = convertBaseSymbols(units)
+        val romanNumeralForUnits = toRoman(units)
         if (i == midPoint - units) {
-            return "${romanNumeralForUnits}${convertBaseSymbols(midPoint)}"
+            return "${romanNumeralForUnits}${toRoman(midPoint)}"
         }
 
-        if (i < midPoint) {
+        if (i < midPoint - 1) {
             return "$romanNumeralForUnits".repeat(i / units)
         }
 
         if (i >= midPoint && i < (units * 10) - units) {
-            return convertBaseSymbols(midPoint) + "$romanNumeralForUnits".repeat(i / units - midPoint / units)
+            return toRoman(midPoint) + "$romanNumeralForUnits".repeat(i / units - midPoint / units)
         }
 
-        return "${romanNumeralForUnits}${convertBaseSymbols(units * 10)}"
+        return "${romanNumeralForUnits}${toRoman(units * 10)}"
     }
 
-    private fun convertBaseSymbols(i: Int): String? {
+    private fun toRoman(i: Int): String? {
         val romanNumerals = mapOf(1 to "I",
+            4 to "IV",
             5 to "V",
+            9 to "IX",
             10 to "X",
+            40 to "XL",
             50 to "L",
+            90 to "XC",
             100 to "C",
+            400 to "CD",
             500 to "D",
+            900 to "CM",
             1000 to "M")
         return romanNumerals[i]
     }
-
-
 }
